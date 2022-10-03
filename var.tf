b20c823f14d10ab822363f23674d83e4a5569aaa
rockettcs <89966003+rockettcs@users.noreply.github.com>
2022-10-03 09:41:38 +0530

    Create var.tf

---
+++ var.tf
@@ -0,0 +1,17 @@
+variable "cidr" {
+  description = "The CIDR block for the VPC. Default value is a valid CIDR, but not acceptable by AWS and should be overridden"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "publicsubnet" {
+  description = "The CIDR block for the VPC. Default value is a valid CIDR, but not acceptable by AWS and should be overridden"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "privatesubnet" {
+  description = "The CIDR block for the VPC. Default value is a valid CIDR, but not acceptable by AWS and should be overridden"
+  type        = string
+  default     = "10.0.2.0/24"
+}
